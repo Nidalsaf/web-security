67a7c6efb89b545f6770e68b2b89cef64f645e2c
Nidalsaf <nidalsaffuri97@gmail.com>
2023-10-25 20:25:32 +0300

    אמיר התותח תודה רבה

--- mysql_conn.php
+++ mysql_conn.php
@@ -6,7 +6,7 @@ class mysql_conn
 
     function __construct(){
         include "db_params.php";
-        $this->conn=new mysqli($db_host, $db_user, $db_pass, $db_schema);
+        $this->conn=new mysqli($db_host, $db_user, $db_pass, $db_schema); 
     }
     function GetConn(){
         return $this->conn;
